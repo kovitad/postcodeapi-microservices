85bbe9f4e0b3250aa9c9e5c543f42d5fe28c0905
Kovitad Janlakhon <kovitadjanlakhon@Kovitads-MBP.lan>
2017-04-12 22:31:23 +1000

    Update Docket package name for Swagger Documentation

--- src/main/java/com/demo/api/postcode/ApplicationConfiguration.java
+++ src/main/java/com/demo/api/postcode/ApplicationConfiguration.java
@@ -31,7 +31,7 @@ public class ApplicationConfiguration {
 	public Docket api(){
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo()).select()                       
-                .apis(RequestHandlerSelectors.basePackage("com.demo.postcode.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.demo.api.postcode.controller"))
                 .paths(PathSelectors.ant("/api/**"))
                 .build();
 				
